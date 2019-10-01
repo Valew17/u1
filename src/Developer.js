f217ec067fb2ffdccf3bcbd65f86356a6294cc9b
Valerii <golem.pm@gmail.com>
2019-10-01 14:27:15 +0300

    Lesson 4 Ok

--- src/Developer.js
+++
@@ -1,7 +0,0 @@
-
-export default class Developer {
-
-    constructor (large) {
-        console.log('Привет ' + large + '! Я из файла Developer!');
-    }
-}
--- src/MenuLi.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class MenuLi extends React.Component {
-
-    render() {
-        return <li><a href={this.props.url}>{this.props.name}</a></li>
-    }
-
-}
